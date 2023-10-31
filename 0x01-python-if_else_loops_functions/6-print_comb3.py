b68bee1cdabaef9de9c2467ecbfac630fae64b86
mahfeshar <mahmoudfeshar11@gmail.com>
2023-10-31 15:32:20 +0200

    Solve Error first time

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(0, 8):
     x = i + 1
     for j in range(x, 10):
-        print(f"{i}{j}", end=", ")
+        print("{}{}".format(i, j), end=", ")
 print(89)
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def islower(C):
     if ord(C) >= 97 and ord(C) <= 122:
         return (True)
