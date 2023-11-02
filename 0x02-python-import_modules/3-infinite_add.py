383c52629eb458d4793acac8bcccd8d50e4f6e03
mahfeshar <mahmoudfeshar11@gmail.com>
2023-11-02 12:31:02 +0200

    Solve errors

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
+    from sys import argv
     sumi = 0;
     for i in range(1, len(argv)):
         sumi += int(argv[i])
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-if i in dir(hidden_4):
-    if i[:2] != "__":
-        print(i)
+if __name__ = "__main__":
+    import hidden_4
+    if i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
