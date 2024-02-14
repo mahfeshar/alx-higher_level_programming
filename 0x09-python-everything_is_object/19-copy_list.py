b82b229d07ff741de9bf3ea6a28a34df6f7a6265
mahfeshar <mahmoudfeshar11@gmail.com>
2024-02-14 22:29:55 +0200

    remove documentation

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-"""Copy function"""
-
-
 def copy_list(l):
-    """Function to copy list
-
-    Args:
-        l (list): list to copy
-
-    Returns:
-        list: copy list
-    """
     return l.copy()
