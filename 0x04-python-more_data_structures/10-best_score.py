7df9b95f8685e4b8c0a61c39d0d698864edfd4ca
mahfeshar <mahmoudfeshar11@gmail.com>
2024-04-16 06:16:24 +0200

    Solve it again

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,17 +2,10 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    key_best = list(a_dictionary)[0]
+    key_best = list(a_dictionary.keys())[0]
     best = a_dictionary[key_best]
     for k, v in list(a_dictionary.items()):
         if v > best:
             best = v
             key_best = k
     return key_best
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
